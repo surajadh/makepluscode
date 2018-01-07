2d5edd6b61c56f1f64bc6173517748bbaa252a8c
Suraj Adhikari <suraj.adhikari@kroger.com>
2018-01-07 16:39:22 -0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Make + Code</h1>
+        </header>
+        <p className="App-intro">
+          We believe: 
+          (Making + Coding + Collaborating) = solving problems
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
